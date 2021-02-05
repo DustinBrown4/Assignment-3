b7841c143f3254152750852c377e8cb41543236e
Dustin Brown <77510407+DustinBrown4@users.noreply.github.com>
2021-02-05 01:05:18 -0500

    Add files via upload

--- Assignment_3_main.cpp
+++ Assignment_3_main.cpp
@@ -15,7 +15,7 @@
 #include "WordArray.h"
 #include "FileReader.h"
 #include <vector>
-
+#include "ErrorCatcher.h"
 #include <stdlib.h> //for random
 
 using namespace std;
@@ -23,10 +23,7 @@ using namespace std;
 
 // declared functions - some overloaded
 void mainMenu();
-bool intMenuInputCheck(int, bool&);
-bool intMenuInputCheck(int, bool&, bool&);
-bool userInputCheck(string&, bool&);
-int wordCounterTracker(bool&, int&);
+
 
 // global objs and vars
 WordArray obj_WordArray;
@@ -60,6 +57,7 @@ void mainMenu() {
 	userInput = false;
 	arrayEmpty = true;
 	menuLoop = true;
+	ErrorCatcher errorCheck;
 
 
 
@@ -72,7 +70,7 @@ void mainMenu() {
 			cout << "\n1 - Add to the Word Array\n2 - View the Word Array\n3 - Remove an entry from Word Array\n0 - Load Hangman program\n";
 			cout << "\nEnter number: ";
 			cin >> choice;
-			intMenuInputCheck(choice, mainMenuInput);  // calls error handler
+			errorCheck.intMenuInputCheck(choice, mainMenuInput);  // calls error handler
 		} while (!mainMenuInput);
 
 		do {
@@ -84,7 +82,7 @@ void mainMenu() {
 				}
 				else {
 					while (wordCount < 11) {
-						wordCounterTracker(userInput, wordCount); // counts wordcount
+						errorCheck.wordCounterTracker(userInput, wordCount); // counts wordcount
 						if (wordCount == 11) {
 							plural = "word";
 						}
@@ -94,7 +92,7 @@ void mainMenu() {
 						cout << "\nAdd 12 words to the Word Array (" << MAX_CHAR_LIMIT << " char limit) " << "\t\t" << (12 - wordCount) <<
 							" " << plural << " remaining\nWord: ";
 						cin >> input;
-						userInputCheck(input, userInput); // error handling
+						errorCheck.userInputCheck(input, userInput); // error handling
 						if (userInput) {
 							arrWords[wordCount] = input; // puts words into array
 						}
@@ -117,7 +115,7 @@ void mainMenu() {
 							"by entering beginning and ending numbers\n0 - Go back to previous menu\n" << endl;
 						cout << "Enter number: ";
 						cin >> choice;
-						intMenuInputCheck(choice, mainMenuInput); // error handling
+						errorCheck.intMenuInputCheck(choice, mainMenuInput); // error handling
 					} while (!mainMenuInput);
 					if (choice == 1) {
 						obj_WordArray.viewArray(arrWords, 12);  // calls object array to view its contents
@@ -127,7 +125,7 @@ void mainMenu() {
 						do {
 							cout << "\nEnter number to display coordinating word\n";
 							cin >> choice;
-							intMenuInputCheck(choice, mainMenuInput, choiceCheck);  //error handling
+							errorCheck.intMenuInputCheck(choice, mainMenuInput, choiceCheck);  //error handling
 							obj_WordArray.viewArray(choice, arrWords, 12);
 						} while (!mainMenuInput);
 					}
@@ -151,7 +149,7 @@ void mainMenu() {
 				break;
 
 			case 0:
-				cout << "\Loading Hangman program . . .\n";
+				cout << "\nLoading Hangman program . . .\n";
 				userInput = true;
 				menuLoop = false;
 				break;
@@ -177,7 +175,7 @@ void mainMenu() {
 			cout << "\n1 - Guess a random word from your Word Array\n2 - Guess a random word from a file\n0 - Exit program\n";
 			cout << "\nEnter number: ";
 			cin >> choice;
-			intMenuInputCheck(choice, mainMenuInput);  // calls error handler
+			errorCheck.intMenuInputCheck(choice, mainMenuInput);  // calls error handler
 		} while (!mainMenuInput);
 		do {
 			switch (choice) {
@@ -269,7 +267,6 @@ void mainMenu() {
 				}
 			}
 
-
 			cout << "\n\t\tfound: " << userFoundCharacters << endl;
 			cout << "\t\twrong: " << wrongCharacters << "\n" << endl;
 			cout << "--------------------------------------------------------------------------------" << endl;
@@ -283,89 +280,6 @@ void mainMenu() {
 
 	cout << "\n\nThank you for using our program!\n\n";
 }
-
-
-
-
-// error handling for first menu interface 1 of 2 similar function - non-overloaded
-
-bool intMenuInputCheck(int choice, bool& intMenuInput) {
-	int check = choice;
-	if (cin.fail()) {
-		cin.clear();
-		string ignoreError;
-		getline(cin, ignoreError);
-	}
-	else if (check != 0 && check != 1 && check != 2 && check != 3) {
-		intMenuInput = false;		// redundant, keeping this here anyway for looks
-	}
-	else {
-		intMenuInput = true;
-		return intMenuInput;
-	}
-	cout << "\n\tInvalid input\nPlease choose only available numbers\n";
-	intMenuInput = false;
-	return intMenuInput;
-}
-
-
-// second part of first error handling for mainmenu interface - overloaded function
-bool intMenuInputCheck(int choice, bool& intMenuInput, bool& choiceCheck) {
-	int check = choice;
-	if (cin.fail()) {
-		cin.clear();
-		string ignoreError;
-		getline(cin, ignoreError);
-	}
-	else if (check < 0 || check > 12) {
-		intMenuInput = false;		// redundant, keeping this here anyway for looks
-	}
-	else {
-		intMenuInput = true;
-		return intMenuInput;
-	}
-	cout << "\n\tInvalid input\nPlease choose only available numbers\n";
-	intMenuInput = false;
-	return intMenuInput;
-}
-
-// second unique error handling for swtich cases -
-bool userInputCheck(string& input, bool& userInput) {
-	string wordError;
-	int wordLength;
-	int limitExceeded;
-
-	getline(cin, wordError);
-	wordLength = input.length();
-	limitExceeded = (wordLength - MAX_CHAR_LIMIT);
-
-	for (int i = 0; i < wordError.length(); i++) {
-		if (isspace(wordError.at(i))) {
-			cout << "\nOne word at a time! Please re-enter word." << endl;
-			userInput = false;
-			return userInput;
-		}
-	}
-
-	if (input.length() > MAX_CHAR_LIMIT) {
-		cout << "\nYou exceeded the 21 maximum character limit by " << limitExceeded << "!" << endl;
-	}
-	else {
-		userInput = true;
-		return userInput;
-	}
-	userInput = false;
-	return userInput;
-}
-
-// simple function to keep track of word count and also used for iteration 
-
-int wordCounterTracker(bool& userInput, int& wordCount) {
-	if (userInput) wordCount++; return wordCount;
-}
-
-
-
 
 
 /*
